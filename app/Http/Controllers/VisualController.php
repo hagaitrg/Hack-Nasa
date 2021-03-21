fb5ccfc9d98037da1119d3ecc3eeb34100267e0d
henryaugusta <henryaugusta4@gmail.com>
2021-03-21 13:16:32 +0700

    slice dashboard -inital 1

--- app/Http/Controllers/VisualController.php
+++ app/Http/Controllers/VisualController.php
@@ -6,5 +6,12 @@ use Illuminate\Http\Request;
 
 class VisualController extends Controller
 {
-    //
+    public function index(){
+
+        return view("layouts.visual.index");
+
+
+    }
+
+
 }
--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 
 Route::post('user/register', 'UserController@store');
+
+
+Route::get('/', 'VisualController@index');
 
 
 Auth::routes();
